69958c913e4217fc940370e47086ab24e7437f6f
Mandy Yeung <mandycodes@gmail.com>
2014-12-13 23:23:06 -0500

    Add animated sticky header

--- js/app.js
+++ js/app.js
@@ -1,5 +1,23 @@
 $(window).load(function() {
-  $('#status').delay(300).fadeOut();
-  $('#preloader').delay(300).fadeOut('slow');
-  $('body').delay(550).css({'overflow':'visible'});
-})
+  $('#status').delay(500).fadeOut();
+  $('#preloader').delay(500).fadeOut('slow');
+  $('body').delay(850).css({'overflow':'visible'});
+});
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 50){
+    $('nav').addClass("sticky");
+    $('#logo').removeClass("hidden");
+  }
+  else{
+    $('nav').removeClass("sticky");
+    $('#logo').addClass("hidden");
+  }
+});
+
+$('#logo').hover(
+  function(){
+    $(this).addClass("animated rubberBand");},
+  function(){
+    $(this).removeClass("animated rubberBand")}
+);
